41aad113ff1f0eda3d2f9ffc75fa3a926fc45904
lmullininx3 <lmullininx3@gatech.edu>
2025-11-23 23:31:25 -0500

    added updated model data, code, and evaluation data

--- backend/nlp_model/code/data_merge.py
+++ backend/nlp_model/code/data_merge.py
@@ -8,8 +8,8 @@ non_terminated_dataset = pd.read_csv(sys.argv[1])
 
 terminated_dataset = pd.read_csv(sys.argv[2])
 
-non_terminated_abstracts = non_terminated_dataset["abstracttext"]
-terminated_abstracts = terminated_dataset["abstracttext"]
+non_terminated_abstracts = non_terminated_dataset["abstract"]
+terminated_abstracts = terminated_dataset["abstract"]
 
 non_terminated_labeled_dataset = []
 
@@ -24,15 +24,25 @@ for a in tqdm(terminated_abstracts):
     row = [1, a]
     terminated_labeled_dataset.append(row)
 
-total_dataset = non_terminated_labeled_dataset + terminated_labeled_dataset
-
 random.seed(42)
+random.shuffle(non_terminated_labeled_dataset)
+random.shuffle(terminated_labeled_dataset)
+
+total_dataset = non_terminated_labeled_dataset[:6000] + terminated_labeled_dataset
+
+# total_dataset = non_terminated_labeled_dataset + terminated_labeled_dataset
+
 random.shuffle(total_dataset)
+
+# print(terminated_labeled_dataset)
 
 header = ["labels", "abstracttext"]   
 
+
 # Create DataFrame
 df = pd.DataFrame(total_dataset, columns=header)
+# df2 = pd.DataFrame(terminated_labeled_dataset, columns=header)
 
 df.to_csv(sys.argv[3], index=False)
+# df2.to_csv(sys.argv[4], index=False)
 
